bf43a36d259afb5293e6fc801b097c89f3ecedc6
scgomes <saulo@id-cotton.com>
2024-03-11 10:21:01 -0400

    Inclusão de script para execução no console do windows

---
+++ kubernetes/startup.bat
@@ -0,0 +1,18 @@
+echo 'Criando o volume...'
+kubectl apply -f volume-pv.yaml
+timeout /t 5
+kubectl apply -f volume-pvc.yaml
+timeout /t 5
+
+echo 'Criando secrets e configMap...'
+kubectl apply -f secrets.yaml
+timeout /t 7
+kubectl apply -f configmap.yaml
+timeout /t 7
+
+echo 'Subindo o banco de dados...'
+kubectl apply -f db-deployment.yaml
+timeout /t 15
+
+echo 'Subindo a aplicação...'
+kubectl apply -f api-deployment.yaml
